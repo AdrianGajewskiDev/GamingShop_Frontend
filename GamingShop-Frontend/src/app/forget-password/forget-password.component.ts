94c291e8f25212f132a71bf996418288503cffb6
adrian.gajewskiDev@outlook.com <adrian.gajewskiDev@outlook.com>
2020-02-29 15:46:42 +0100

    Styled the reset password page

--- GamingShop-Frontend/src/app/forget-password/forget-password.component.ts
+++ GamingShop-Frontend/src/app/forget-password/forget-password.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 import { UserService } from "../shared/user.service";
+import { ToastrService } from "ngx-toastr";
 @Component({
   selector: "app-forget-password",
   templateUrl: "./forget-password.component.html",
   styleUrls: ["./forget-password.component.css"]
 })
 export class ForgetPasswordComponent implements OnInit {
-  constructor(private formBuilder: FormBuilder, private service: UserService) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private service: UserService,
+    private toastr: ToastrService
+  ) {}
   formGroup: FormGroup;
   ngOnInit() {
     this.formGroup = this.formBuilder.group({
@@ -16,8 +21,15 @@ export class ForgetPasswordComponent implements OnInit {
   }
 
   onSubmit() {
-    this.service
-      .forgetPassword(this.formGroup.get("Email").value)
-      .subscribe(res => console.log(res));
+    this.service.forgetPassword(this.formGroup.get("Email").value).subscribe(
+      (res: any) => {
+        this.toastr.info(
+          "We've send you a password reset link, check your email box"
+        );
+      },
+      error => {
+        console.log(error);
+      }
+    );
   }
 }
